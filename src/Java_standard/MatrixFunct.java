5b3410da5c0137265bacf2cafc6a7447deb9176d
Ulises Castro <ulises.castro@gmail.com>
2021-03-11 16:23:58 -0400

    Added strMatrix to represent matrix as String

--- src/Java_standard/MatrixFunct.java
+++ src/Java_standard/MatrixFunct.java
@@ -10,13 +10,13 @@ package Java_standard;
 public class MatrixFunct {
 
 	/**
-	 * <pre><b>public int[][]</b> multiplyMatrix(int[][] matrix1, int[][] matrix2)</pre>
+	 * <pre><b>public static int[][]</b> multiplyMatrix(int[][] matrix1, int[][] matrix2)</pre>
 	 * Multiplies two matrix, for which the second matrix should have the same
 	 * number of rows as the number of columns of the first matrix.<br><br>
 	 * @param matrix1 First matrix to multiply 
 	 * @param matrix2 Second matrix to multiply by
 	 */
-	public int[][] multiplyMatrix(int[][] matrix1, int[][] matrix2) {
+	public static int[][] multiplyMatrix(int[][] matrix1, int[][] matrix2) {
 		if (cols(matrix1)!=rows(matrix2)) return null;
 		int[][] result = new int[rows(matrix1)][cols(matrix2)];
 		for (int i=0; i<rows(matrix1); i++) {
@@ -29,12 +29,12 @@ public class MatrixFunct {
 	}
 	
 	/**
-	 * <pre><b>public int[][]</b> scalarMatrix(int[][] matrix, int multiple)</pre>
+	 * <pre><b>public static int[][]</b> scalarMatrix(int[][] matrix, int multiple)</pre>
 	 * Multiplies each value of the matrix for some scalar number.<br><br> 
 	 * @param matrix Matrix to escalate 
 	 * @param multiple Valor to multiply by
 	 */
-	public int[][] scalarMatrix(int[][] matrix, int multiple) {
+	public static int[][] scalarMatrix(int[][] matrix, int multiple) {
 		int[][] result = new int[rows(matrix)][cols(matrix)];
 		for (int i=0; i<matrix.length; i++) {
 			for (int j=0; j<matrix[0].length; j++){
@@ -45,13 +45,13 @@ public class MatrixFunct {
 	}
 	
 	/**
-	 * <pre><b>public int[][]</b> sumMatrix(int[][] matrix1, int[][] matrix2)</pre>
+	 * <pre><b>public static int[][]</b> sumMatrix(int[][] matrix1, int[][] matrix2)</pre>
 	 * Sums two matrix generating another matrix.<br><br>
 	 * @param matrix1 first matrix to add
 	 * @param matrix2 second matrix to add
 	 * @return Returns the resulting matrix of the addition operation.
 	 */
-	public int[][] sumMatrix(int[][] matrix1, int[][] matrix2) { 
+	public static int[][] sumMatrix(int[][] matrix1, int[][] matrix2) { 
 		int rows = max(rows(matrix1),rows(matrix2));
 		int cols = max(cols(matrix1),cols(matrix2));
 		int[][] result = new int[rows][cols];
@@ -66,23 +66,23 @@ public class MatrixFunct {
 	}
 	
 	/**
-	 * <pre><b>public int[][]</b> sumMatrix(int[][] matrix1, int[][] matrix2)</pre>
+	 * <pre><b>public static int[][]</b> sumMatrix(int[][] matrix1, int[][] matrix2)</pre>
 	 * Subtraction of two matrix generating another matrix.<br><br>
 	 * @param matrix1 first matrix to subtract
 	 * @param matrix2 second matrix to subtract
 	 * @return Returns the resulting matrix of the subtraction operation.
 	 */
-	public int[][] subtractMatrix(int[][] matrix1, int[][] matrix2) {
+	public static int[][] subtractMatrix(int[][] matrix1, int[][] matrix2) {
 		return sumMatrix(matrix1, scalarMatrix(matrix2, -1));
 	}
 	
 	/**
-	 * <pre><b>public int[][]</b> transformMatrix(int[][] matrix)</pre>
+	 * <pre><b>public static int[][]</b> transformMatrix(int[][] matrix)</pre>
 	 * Returns the matrix transposed.<br><br>
 	 * @param matrix Original matrix to transpose
 	 * @return	Transpose of the original matrix.
 	 */
-	public int[][] transposeMatrix(int[][] matrix) {
+	public static int[][] transposeMatrix(int[][] matrix) {
 		int[][] result = new int[cols(matrix)][rows(matrix)];
 			for (int i=0; i<cols(matrix); i++) {
 				for(int j=0; j<rows(matrix); j++) {
@@ -93,22 +93,22 @@ public class MatrixFunct {
 	}
 	
 	/**
-	 * <pre><b>public boolean</b> isSquared(int[][] matrix)</pre>
+	 * <pre><b>public static boolean</b> isSquared(int[][] matrix)</pre>
 	 * Indicates if a matrix has the same number of rows and columns.<br><br>
 	 * @param matrix Matrix to evaluate
 	 * @return condition if the matrix is squared.
 	 */
-	public boolean isSquared(int[][] matrix) {
+	public static boolean isSquared(int[][] matrix) {
 		return rows(matrix)==cols(matrix);
 	}
 	
 	/**
-	 * <pre><b>public boolean</b> isDiagonal(int[][] matrix)</pre>
+	 * <pre><b>public static boolean</b> isDiagonal(int[][] matrix)</pre>
 	 * Indicates if all the element outside the diagonal of the matrix are zeros.<br><br>
 	 * @param matrix Matrix to evaluate
 	 * @return condition if the matrix is diagonal.
 	 */
-	public boolean isDiagonal(int[][] matrix) {
+	public static boolean isDiagonal(int[][] matrix) {
 		for (int i=0; i<rows(matrix); i++) {
 			for (int j=0; j<cols(matrix); j++) {
 				if ((i!=j)&&(matrix[i][j]!=0)) return false;
@@ -118,12 +118,12 @@ public class MatrixFunct {
 	}
 	
 	/**
-	 * <pre><b>public boolean</b> isUpperTriang(int[][] matrix)</pre>
+	 * <pre><b>public static boolean</b> isUpperTriang(int[][] matrix)</pre>
 	 * Indicates if all the element below the diagonal of the matrix are zeros.<br><br>
 	 * @param matrix Matrix to evaluate
 	 * @return condition if the matrix is upper triangular.
 	 */
-	public boolean isUpperTriang(int[][] matrix) {
+	public static boolean isUpperTriang(int[][] matrix) {
 		for (int i=0; i<rows(matrix); i++) {
 			for (int j=0; j<cols(matrix); j++) {
 				if ((i>j)&&(matrix[i][j]!=0)) return false;
@@ -133,12 +133,12 @@ public class MatrixFunct {
 	}
 
 	/**
-	 * <pre><b>public boolean</b> isLowerTriang(int[][] matrix)</pre>
+	 * <pre><b>public static boolean</b> isLowerTriang(int[][] matrix)</pre>
 	 * Indicates if all the element above the diagonal of the matrix are zeros.<br><br>
 	 * @param matrix Matrix to evaluate
 	 * @return condition if the matrix is lower triangular.
 	 */
-	public boolean isLowerTriang(int[][] matrix) {
+	public static boolean isLowerTriang(int[][] matrix) {
 		for (int i=0; i<rows(matrix); i++) {
 			for (int j=0; j<cols(matrix); j++) {
 				if ((i<j)&&(matrix[i][j]!=0)) return false;
@@ -154,7 +154,7 @@ public class MatrixFunct {
 	 * @param y Second value to evaluate
 	 * @return Returns the maximum of the two values
 	 */
-	private int max(int x, int y) {
+	private static int max(int x, int y) {
 		if (x >= y) return x;
 		return y;
 	}
@@ -165,7 +165,7 @@ public class MatrixFunct {
 	 * @param matrix Matrix to evaluate.
 	 * @return the number of rows.
 	 */
-	private int rows(int[][] matrix) {
+	private static int rows(int[][] matrix) {
 		return matrix.length;
 	}
 	
@@ -175,7 +175,7 @@ public class MatrixFunct {
 	 * @param matrix Matrix to evaluate.
 	 * @return the number of columns.
 	 */
-	private int cols(int[][] matrix) {
+	private static int cols(int[][] matrix) {
 		return matrix[0].length;
 	}
 	
@@ -187,7 +187,7 @@ public class MatrixFunct {
 	 * @param index Index of the row to take.
 	 * @return The row specified by the index.
 	 */
-	public int[] row(int[][] matrix, int index) {
+	public static int[] row(int[][] matrix, int index) {
 		if (index > rows(matrix)) return null;
 		int[] row = new int[cols(matrix)];
 		for (int i=0; i<cols(matrix); i++) {
@@ -204,7 +204,7 @@ public class MatrixFunct {
 	 * @param index Index of the column to take.
 	 * @return The column specified by the index.
 	 */
-	public int[] col(int[][] matrix, int index) {
+	public static int[] col(int[][] matrix, int index) {
 		if (index > cols(matrix)) return null;
 		int[] col = new int[rows(matrix)];
 		for (int i=0; i<rows(matrix); i++) {
@@ -220,13 +220,32 @@ public class MatrixFunct {
 	 * @param arr2 Second array to multiply
 	 * @return Result of the multiplication of both arrays.
 	 */
-	public Integer multiplyArr(int[] arr1, int[] arr2) {
+	public static Integer multiplyArr(int[] arr1, int[] arr2) {
 		if (arr1.length!=arr2.length) return null;
 		int result = 0;
 		for (int i=0; i<arr1.length; i++) {
 			result += arr1[i] * arr2[i];
 		}
 		return result;
+	}
+
+	/**
+	 * 
+	 */
+	public static String strMatrix(int[][] matrix) {
+		String s = "[ [ ";
+		int cols = cols(matrix);
+		int rows = rows(matrix);
+
+		for (int i=0; i<rows; i++) {
+			for (int j=0; j<cols; j++) {
+				s += String.format("%3d",matrix[i][j]);
+				if (j<(cols-1)) s+=", ";  
+			}
+			if (i<(rows-1)) s+=" ] \n  [ ";
+				else s+=" ] ]";
+		}
+		return s;				
 	}
 	
 }
